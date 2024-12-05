17964de1f4d1126c9b415b2bde9d2dc772d00327
Nikita <enagtim@mail.ru>
2024-12-05 21:38:06 +0300

    Приложение почищено, добавлен линтер

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,16 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    hmr: {
-      protocol: 'ws',
-      host: 'localhost',
-      port: 3100
-    },
-    open: true,
-    watch: {
-      usePolling: true
-    }
-  }
+	plugins: [react()],
+	server: {
+		hmr: {
+			protocol: 'ws',
+			host: 'localhost',
+			port: 3100,
+		},
+		open: true,
+		watch: {
+			usePolling: true,
+		},
+	},
 });
